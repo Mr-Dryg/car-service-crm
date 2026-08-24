056fc849ddd556316d10d111571a9ab74e221e19
Mr-Dryg <shirobockow.dim@yandex.ru>
2026-08-24 19:34:38 +0300

    feat(app): add database connection check
    Closes #7

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/Mr-Dryg/car-service-crm/internal/config"
+	"github.com/Mr-Dryg/car-service-crm/internal/repository/postgres"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("loading dotenv with error: %v", err)
+	}
+
+	cnf, err := config.Load()
+	if err != nil {
+		log.Fatalf("loading config with error: %v", err)
+	}
+
+	ctx := context.Background()
+	pool, err := postgres.NewPool(ctx, cnf.DatabaseURL)
+	if err != nil {
+		log.Fatalf("creating pool with error: %v", err)
+	}
+	defer pool.Close()
+
+	log.Println("success connection to db")
 }
