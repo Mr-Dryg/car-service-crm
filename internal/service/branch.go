4a761a162a2ae663223fa53a8862b0275834fe17
Mr-Dryg <shirobockow.dim@yandex.ru>
2026-08-25 00:32:40 +0300

    refactor(service): move interface to branch.go

--- internal/service/branch.go
+++ internal/service/branch.go
@@ -6,6 +6,11 @@ import (
 	"github.com/Mr-Dryg/car-service-crm/internal/domain"
 )
 
+type BranchRepository interface {
+	Create(ctx context.Context, branch *domain.Branch) error
+	GetAll(ctx context.Context) ([]domain.Branch, error)
+}
+
 type BranchService struct {
 	branchRepo BranchRepository
 }
